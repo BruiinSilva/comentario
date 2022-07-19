e519998a5b7ad3c24c2ec150930ef8f76dcbcd60
Bruno C. Gomes Da Silva <73727546+BruiinSilva@users.noreply.github.com>
2022-07-19 17:42:13 -0300

    Página para comentários

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+function enviar() {
+
+    // Variáveis
+    var corFundo = document.getElementById("cor-fundo").value;
+    var corTexto = document.getElementById("cor-texto").value;
+    var comentario = document.getElementById("comentario").value;
+    var nome = document.getElementById("nome").value;
+    
+  
+    // Mandando pro HTML
+    document.getElementById("ana2").innerHTML += '<div class="usuario" style="color: ' + corTexto + ';background-color: ' + corFundo + ';" <h4>' + nome + '</h4><hr style="border-color: ' + corTexto + ';"><p>' + comentario + '</p></div>';
+  }
